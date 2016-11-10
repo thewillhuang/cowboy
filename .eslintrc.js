4e6f67f8a7543b81adc92f53b335f5525fd6054b
William <will.h86@gmail.com>
2016-11-09 23:51:53 -0800

    nuke some stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,6 @@
 module.exports = {
-  parser: 'babel-eslint',
+  extends: ['airbnb-base'],
   env: {
     node: true,
-  },
-  rules: {
-    'generator-star-spacing': 0,
-    'babel/generator-star-spacing': 1,
-    'jsx-a11y/anchor-has-content': [ 2, {
-      'components': [ 'Anchor' ],
-    }],
-  },
-  extends: ['airbnb'],
-  plugins: [
-    'babel',
-  ],
+  }
 }
