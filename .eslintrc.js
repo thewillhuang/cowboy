5671fda172f4a9f314e40fa3ac02d405dd0f8b92
William <will.h86@gmail.com>
2016-11-09 23:38:37 -0800

    changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 module.exports = {
-  extends: ['airbnb'],
   parser: 'babel-eslint',
   env: {
     node: true,
@@ -7,7 +6,11 @@ module.exports = {
   rules: {
     'generator-star-spacing': 0,
     'babel/generator-star-spacing': 1,
+    'jsx-a11y/anchor-has-content': [ 2, {
+      'components': [ 'Anchor' ],
+    }],
   },
+  extends: ['airbnb'],
   plugins: [
     'babel',
   ],
