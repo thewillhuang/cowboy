2477c885d3c54988fd4fe5d715838594bbd730e7
William <will.h86@gmail.com>
2016-11-10 16:25:31 -0800

    load node_modules

--- functions/requester-apex/src/index.js
+++ functions/requester-apex/src/index.js
@@ -1,6 +1,8 @@
+import gm from 'gm';
 import axios from 'axios';
 import λ from 'apex.js';
 
+console.log(gm);
 export default λ(e =>
   Promise.all(e.urls.map(async (url) => {
     console.log('fetching %s', url);
