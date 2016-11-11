48d5dcaf26e150d10509f093d77d80e9631ed938
William <will.h86@gmail.com>
2016-11-11 11:05:58 -0800

    small change

--- functions/requester-apex/src/index.js
+++ functions/requester-apex/src/index.js
@@ -3,7 +3,7 @@ import λ from 'apex.js';
 
 export default λ(e =>
   Promise.all(e.urls.map(async (url) => {
-    console.log('fetching %s', url);
+    console.log(`fetching ${url}`);
     return {
       status: (await axios.get(url)).status,
       url,
