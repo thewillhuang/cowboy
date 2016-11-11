ac1512c4d15614f3f493e8ee7cecaf14c20290bc
William <will.h86@gmail.com>
2016-11-10 16:26:54 -0800

    remove a file

--- functions/requester-apex/src/index.js
+++ functions/requester-apex/src/index.js
@@ -1,8 +1,6 @@
-import gm from 'gm';
 import axios from 'axios';
 import λ from 'apex.js';
 
-console.log(gm);
 export default λ(e =>
   Promise.all(e.urls.map(async (url) => {
     console.log('fetching %s', url);
