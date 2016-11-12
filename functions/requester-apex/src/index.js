3b13b68ee7e04ce47612eecaeb490cb5c72adcc9
William <will.h86@gmail.com>
2016-11-12 14:33:26 -0800

    try scrypt

--- functions/requester-apex/src/index.js
+++ functions/requester-apex/src/index.js
@@ -1,8 +1,21 @@
 import axios from 'axios';
 import λ from 'apex.js';
+import { paramsSync, kdf, verifyKdf } from 'scrypt';
+
+const scryptParam = paramsSync(0.025);
+
+const generateHash = password =>
+  kdf(Buffer.from(password), scryptParam)
+    .then(result => result.toString('base64'))
+    .catch(e => console.log(e));
+
+// returns true or false given password and stored hash
+const validatePassword = (password, hash) =>
+  verifyKdf(Buffer.from(hash, 'base64'), Buffer.from(password))
+    .catch(e => console.log(e));
 
 export default λ(e =>
   Promise.all(e.urls.map(async url => ({
-    status: (await axios.get(url)).status,
+    status: await generateHash(url),
     url,
   }))));
