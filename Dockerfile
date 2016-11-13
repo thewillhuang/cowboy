3a134e64a04ae0ef6de96b52ed04590dbda76827
William <will.h86@gmail.com>
2016-11-13 09:57:18 -0800

    minimize

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM lambci/lambda:build
-
-RUN curl -o- -L https://yarnpkg.com/install.sh | bash
-
-COPY ./package.json .
-
-RUN $HOME/.yarn/bin/yarn install
--- build.sh
+++
@@ -1,9 +0,0 @@
-# # on lambci
-# $HOME/.yarn/bin/yarn install;
-# yarn;
-# ../../node_modules/.bin/webpack --config ../../webpack.config.js;
-
-# # on local
-# docker build -t lambda .;
-# docker run lambda;
-# docker cp $(docker ps -alq):/var/task/node_modules ./node_modules;
