9cbfbbcd59f022b1c4582f9e3cc7cd40082efda6
William <will.h86@gmail.com>
2016-12-02 17:51:59 -0800

    typeof body

--- functions/graphql/src/index.js
+++ functions/graphql/src/index.js
@@ -1,13 +1,14 @@
 import λ from 'apex.js';
-// import { paramsSync, kdf } from 'scrypt';
+import { paramsSync, kdf } from 'scrypt';
 
-// const scryptParam = paramsSync(0.025);
+const scryptParam = paramsSync(0.025);
 
-// const generateHash = password =>
-//   kdf(new Buffer(password), scryptParam)
-//     .then(result => result.toString('base64'));
+const generateHash = password =>
+  kdf(new Buffer(password), scryptParam)
+    .then(result => result.toString('base64'));
 
 export default λ(async (e) => {
   const { body } = e;
-  return typeof body;
+  console.log(typeof body);
+  return await generateHash('okay');
 });
