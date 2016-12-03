399048387e2c3437fe148058099f259291cda056
William <will.h86@gmail.com>
2016-12-02 17:42:28 -0800

    updates

--- functions/graphql/src/index.js
+++ functions/graphql/src/index.js
@@ -1,14 +1,14 @@
 import λ from 'apex.js';
-import { paramsSync, kdf } from 'scrypt';
+// import { paramsSync, kdf } from 'scrypt';
 
-const scryptParam = paramsSync(0.025);
+// const scryptParam = paramsSync(0.025);
 
-const generateHash = password =>
-  kdf(new Buffer(password), scryptParam)
-    .then(result => result.toString('base64'));
+// const generateHash = password =>
+//   kdf(new Buffer(password), scryptParam)
+//     .then(result => result.toString('base64'));
 
 export default λ(async (e) => {
-  const { body: { password } } = e;
-  console.log('password', password);
-  return await generateHash(password);
+  const { body } = e;
+  console.log(typeof body);
+  return true;
 });
